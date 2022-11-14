36acf82b2cbb1ea44bcff6222f3237972fe41ddd
Steven Nguyen <steven@boostcommerce.net>
2022-11-14 20:44:20 +0700

    feat: config prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
-import './globals.css'
+import './globals.css';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head />
-      <body>{children}</body>
+      <body>
+        <div>{children}</div>
+      </body>
     </html>
-  )
+  );
 }
